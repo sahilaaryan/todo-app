482cba21b8aff37181242dc365f07783fec232d0
sahilaaryan <135539117+sahilaaryan@users.noreply.github.com>
2023-11-14 16:15:25 +0530

    Add files via upload

---
+++ React-Redux-Todo-App-main/src/components/DisplayTodos.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import {
+  addTodos,
+  completeTodos,
+  removeTodos,
+  updateTodos,
+} from "../redux/reducer";
+import TodoItem from "./TodoItem";
+import { AnimatePresence } from "framer-motion";
+import { Button } from "@mui/material";
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+    removeTodo: (id) => dispatch(removeTodos(id)),
+    updateTodo: (obj) => dispatch(updateTodos(obj)),
+    completeTodo: (id) => dispatch(completeTodos(id)),
+  };
+};
+
+const DisplayTodos = (props) => {
+  const [sort, setSort] = useState("active");
+  return (
+    <div className="displaytodos">
+      <div className="buttons">
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "#271c6c"
+          }}
+          onClick={() => setSort("active")}
+        >
+          Active
+        </Button>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "#271c6c"
+          }}
+          onClick={() => setSort("completed")}
+        >
+          Completed
+        </Button>
+        <Button
+          sx={{
+            backgroundColor: "#271c6c"
+          }}
+          variant="contained"
+          onClick={() => setSort("all")}
+        >
+          All
+        </Button>
+      </div>
+      <ul>
+        <AnimatePresence>
+          {props.todos.length > 0 && sort === "active"
+            ? props.todos.map((item) => {
+                return (
+                  item.completed === false && (
+                    <TodoItem
+                      key={item.id}
+                      item={item}
+                      removeTodo={props.removeTodo}
+                      updateTodo={props.updateTodo}
+                      completeTodo={props.completeTodo}
+                    />
+                  )
+                );
+              })
+            : null}
+          {/* for completed items */}
+          {props.todos.length > 0 && sort === "completed"
+            ? props.todos.map((item) => {
+                return (
+                  item.completed === true && (
+                    <TodoItem
+                      key={item.id}
+                      item={item}
+                      removeTodo={props.removeTodo}
+                      updateTodo={props.updateTodo}
+                      completeTodo={props.completeTodo}
+                    />
+                  )
+                );
+              })
+            : null}
+          {/* for all items */}
+          {props.todos.length > 0 && sort === "all"
+            ? props.todos.map((item) => {
+                return (
+                  <TodoItem
+                    key={item.id}
+                    item={item}
+                    removeTodo={props.removeTodo}
+                    updateTodo={props.updateTodo}
+                    completeTodo={props.completeTodo}
+                  />
+                );
+              })
+            : null}
+        </AnimatePresence>
+      </ul>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayTodos);
